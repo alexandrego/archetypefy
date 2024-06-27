6811e29c24a0e662eabe7be40138e4d2296f7c82
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2024-06-26 22:04:04 -0300

    Ajustando Layout do Email

--- resources/views/layouts/mail/mail.blade.php
+++ resources/views/layouts/mail/mail.blade.php
@@ -10,18 +10,44 @@
 
         display: flex;
         flex-direction: column;
-        align-items: center;">
+        align-items: center;
+    ">
 
-    <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="150" />
-    <p style="
-        font-size:16px;
-        text-align:justify;">
-        Olá, {{ session('nome') }}, se você tentou acessar sua conta conosco segue abaixo seu código para acesso a sua conta, se não foi você pode desconsiderar esse e-mail, alguém pode ter digitado o e-mail por engano.</p>
-    <!-- <p style="font-size: 18px;">Seu E-mail: {{ session('email') }}</p>
-    <p style="font-size: 18px;">Seu Celular: {{ session('mobile') }}</p> -->
-    <br>
-    <p style="font-size: 14px;">Código de confirmação: {{ session('confirmCode') }}</p>
-    <br>
-    <p style="font-size: 12px;">Este é um código único que espira em 2 minutos, então fique tranquilo que estamos cuidando da segurança da sua conta.</p>
+    <div style="
+            width: 100%;
+        ">
+        <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="150" />
+    </div>
+
+    <div style="
+            width: 100%;
+            font-size:16px;
+            text-align:justify;
+        ">
+        <p>
+            Olá, {{ session('nome') }}, se você tentou acessar sua conta conosco segue abaixo seu código para acesso a sua conta, se não foi você pode desconsiderar esse e-mail, alguém pode ter digitado o e-mail por engano.
+        </p>
+    </div>
+
+    <div style="
+            width: 100%;
+            margin: 20px 0;
+            font-size:14px;
+            text-align:justify;
+        ">
+        <p>
+            Código de confirmação: {{ session('confirmCode') }}
+        </p>
+    </div>
+
+    <div style="
+            width: 100%;
+            font-size:12px;
+            text-align:justify;
+        ">
+        <p>
+            Este é um código único que espira em 2 minutos, então fique tranquilo que estamos cuidando da segurança da sua conta.
+        </p>
+    </div>
 
 </div>
