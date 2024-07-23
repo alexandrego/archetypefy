b3a7cb2cfae20a62f25c10c07e03e0d83ad28fda
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2024-07-22 23:38:09 -0300

    chore: Orfão e Governante

--- resources/views/layouts/mail/mailPdfResult.blade.php
+++ resources/views/layouts/mail/mailPdfResult.blade.php
@@ -6,21 +6,21 @@
     <title>Resultado do Arquétipo</title>
 </head>
 <body>
-    <div style="max-width:100vw;font-family:Arial, Helvetica, sans-serif;padding:20px;line-height:1.5;letter-spacing:0.6px;background-color:#000;color:#fff;">
+    <div style="max-width:100vw;font-family:Arial, Helvetica, sans-serif;padding:20px;line-height:1.5;letter-spacing:0.6px;background-color:#fff;color:#000;">
 
     <div style="width:100%;display:flex;flex-direction:column;align-items:center;">
         <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="150" />
     </div>
 
-    <div style="width:100%;font-size:16px;text-align:justify;color:#fff;">
+    <div style="width:100%;font-size:16px;text-align:justify;color:#000;">
             Olá, {{ session('fullName') }}, Parabéns por completar o seu teste, agora você esta pronto(a) para começar a caminhar na direção certa.
     </div>
 
-    <div style="width:100%;margin:20px 0;font-size:14px;align-items:center;color:#fff;">
+    <div style="width:100%;margin:20px 0;font-size:14px;align-items:center;color:#000;">
             Seu principal arquétipo é: {{ session('resultadoFinal') }}
     </div>
 
-    <div style="width:100%;font-size:12px;text-align:justify;color:#fff;">
+    <div style="width:100%;font-size:12px;text-align:justify;color:#000;">
         @if(session('resultadoFinal') == 'Orfão')
             <p>
                 O arquétipo {{ session('resultadoFinal') }} descreve uma pessoa que busca pertencimento e segurança, enfrentando os desafios da vida com resiliência e realismo. Muitas vezes cético, o Órfão anseia por conexões autênticas e um senso de comunidade.
