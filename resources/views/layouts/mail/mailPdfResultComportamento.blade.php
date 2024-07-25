f57a2118c198d83803c7e83961dae83daf208cdf
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2024-07-25 01:26:29 -0300

    chore: Teste de Arquétipos Finalizado ✨

--- resources/views/layouts/mail/mailPdfResultComportamento.blade.php
+++ resources/views/layouts/mail/mailPdfResultComportamento.blade.php
@@ -6,21 +6,21 @@
     <title>Resultado do Perfil Comportamental</title>
 </head>
 <body>
-    <div style="max-width:100vw;font-family:Arial, Helvetica, sans-serif;padding:20px;line-height:1.5;letter-spacing:0.6px;background-color:#000;color:#fff;">
+    <div style="max-width:100vw;font-family:Arial, Helvetica, sans-serif;padding:20px;line-height:1.5;letter-spacing:0.6px;background-color:#fff;color:#000;">
 
     <div style="width:100%;display:flex;flex-direction:column;align-items:center;">
-        <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="150" />
+        <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="100" />
     </div>
 
-    <div style="width:100%;font-size:16px;text-align:justify;color:#fff;">
+    <div style="width:100%;font-size:16px;text-align:justify;color:#000;">
             Olá, {{ session('fullName') }}, Parabéns por completar o seu teste, agora você esta pronto(a) para começar a caminhar na direção certa.
     </div>
 
-    <div style="width:100%;margin:20px 0;font-size:14px;align-items:center;color:#fff;">
-            Seu comportamento principal é: {{ session('resultadoFinalComportamento') }}
+    <div style="width:100%;margin:20px 0;font-size:16px;align-items:center;color:#000;">
+            Seu comportamento principal é: <span style="font-size:18px"><strong>{{ session('resultadoFinalComportamento') }}</strong>
     </div>
 
-    <div style="width:100%;font-size:12px;text-align:justify;color:#fff;">
+    <div style="width:100%;font-size:15px;text-align:justify;color:#000;">
             O comportamento {{ session('resultadoFinalComportamento') }} descreve uma pessoa que é....
     </div>
 
