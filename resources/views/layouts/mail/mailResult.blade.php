63fce4cc87ec63ba9a5645a155fca861af1de443
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2024-07-25 01:13:34 -0300

    chore: Teste de Arquétipos Finalizado ✨

--- resources/views/layouts/mail/mailResult.blade.php
+++ resources/views/layouts/mail/mailResult.blade.php
@@ -1,7 +1,7 @@
 <div style="max-width:100vw;font-family:Arial, Helvetica, sans-serif;padding:20px;line-height:1.5;letter-spacing:0.6px;background-color:#fff;color:#000;">
 
     <div style="width:100%;display:flex;flex-direction:column;align-items:center;">
-        <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="150" />
+        <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="100" />
     </div>
 
     <div style="width:100%;font-size:16px;text-align:justify;color:#000;">
