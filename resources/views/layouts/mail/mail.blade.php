98e7a0800e4cb1d1453f435197f07f239f47d48c
Alexandre Goncalves <desenvolvimento@diamondnautica.com.br>
2024-08-08 13:44:07 -0300

    chore: Corrigindo e-mail não encontrado no banco

--- resources/views/layouts/mail/mail.blade.php
+++ resources/views/layouts/mail/mail.blade.php
@@ -1,19 +1,29 @@
-<div style="max-width:100vw;font-family:Arial, Helvetica, sans-serif;padding:20px;line-height:1.5;letter-spacing:0.6px;background-color:#fff;color:#000;">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Archetypefy - Código de segurança</title>
+</head>
+<body>
+    <div style="max-width:100vw;font-family:Arial, Helvetica, sans-serif;padding:20px;line-height:1.5;letter-spacing:0.6px;background-color:#fff;color:#000;">
 
-    <div style="width:100%;display:flex;flex-direction:column;align-items:center;">
-        <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="150" />
+        <div style="width:100%;display:flex;flex-direction:column;align-items:center;">
+            <img src="https://archetypefy.com.br/wp-content/webp-express/webp-images/uploads/2024/06/LOGO_Prancheta-1-1024x1024.png.webp" width="100" />
+        </div>
+
+        <div style="width:100%;font-size:16px;text-align:justify;color:#000;">
+                Olá {{ $userName }}, se você tentou acessar sua conta conosco segue abaixo seu código para acesso a sua conta, se não foi você pode desconsiderar esse e-mail, alguém pode ter digitado o e-mail por engano.
+        </div>
+
+        <div style="width:100%;margin:20px 0;font-size:14px;align-items:center;color:#000;">
+                Código de confirmação: <strong>{{ $confirmCode }}</strong>
+        </div>
+
+        <div style="width:100%;font-size:12px;text-align:justify;color:#000;">
+                Este é um código único que espira em 2 minutos, então fique tranquilo que estamos cuidando da segurança da sua conta.
+        </div>
+
     </div>
-
-    <div style="width:100%;font-size:16px;text-align:justify;color:#000;">
-            Olá {{ $userName }}, se você tentou acessar sua conta conosco segue abaixo seu código para acesso a sua conta, se não foi você pode desconsiderar esse e-mail, alguém pode ter digitado o e-mail por engano.
-    </div>
-
-    <div style="width:100%;margin:20px 0;font-size:14px;align-items:center;color:#000;">
-            Código de confirmação: <strong>{{ $confirmCode }}</strong>
-    </div>
-
-    <div style="width:100%;font-size:12px;text-align:justify;color:#000;">
-            Este é um código único que espira em 2 minutos, então fique tranquilo que estamos cuidando da segurança da sua conta.
-    </div>
-
-</div>
+</body>
+</html>
