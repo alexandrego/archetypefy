67419604efda41cf0f22c8f437ba6d758163bb01
Alexandre Goncalves <desenvolvimento@diamondnautica.com.br>
2024-08-14 15:12:39 -0300

    chore: Ajustando link do suporte

--- resources/views/components/suporte.blade.php
+++ resources/views/components/suporte.blade.php
@@ -5,7 +5,12 @@
     <div class="suportePhoto">
         <img src="src/assets/img/suporte.svg" />
     </div>
-    <div class="suporteAction">
-        Conversar via WhatsApp
-    </div>
+
+    {{-- +55 11 3042-9643 --}}
+
+    <a href="https://wa.me/551130429643?text=Estou%20com%20dúvida%20aqui%20no%20teste!">
+        <div class="suporteAction">
+            Conversar via WhatsApp
+        </div>
+    </a>
 </div>
