b72ad17306538d218783e1d0ef9ee1aa24cb6fe7
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2024-08-13 23:53:45 -0300

    chore: Criando componente suporte

---
+++ resources/views/components/suporte.blade.php
@@ -0,0 +1,11 @@
+<div class="suporte">
+    <div class="suporteName">
+        Suporte
+    </div>
+    <div class="suportePhoto">
+        <img src="src/assets/img/suporte.svg" />
+    </div>
+    <div class="suporteAction">
+        Conversar via WhatsApp
+    </div>
+</div>
--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -576,17 +576,7 @@
                     {{-- </a> --}}
                 </div>
 
-                <div class="suporte">
-                    <div class="suporteName">
-                        Suporte
-                    </div>
-                    <div class="suportePhoto">
-                        <img src="src/assets/img/suporte.svg" />
-                    </div>
-                    <div class="suporteAction">
-                        Conversar via WhatsApp
-                    </div>
-                </div>
+                <x-suporte />
 
             </div>
         </div>
