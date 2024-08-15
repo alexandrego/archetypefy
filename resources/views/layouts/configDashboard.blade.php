4332e2dfa00022ab1ff9f9334fc50c4040af4914
Alexandre Goncalves <desenvolvimento@diamondnautica.com.br>
2024-08-15 17:54:25 -0300

    chore: Dashboard Controle De Usuário

--- resources/views/layouts/configDashboard.blade.php
+++ resources/views/layouts/configDashboard.blade.php
@@ -62,7 +62,7 @@
                         </div>
 
                         <!-- Modal Adicionar -->
-                        <div class="modalAdicionar fade" id="modalExemploAdicionar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="modalExemploAdicionar" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
