8b74e742e095876f0ce369009f17cd96bcc61538
Alexandre Gonçalves <alexandre_c_g@hotmail.com>
2024-08-17 20:48:09 -0300

    chore: Resgatando dados do usuário

--- resources/views/layouts/configDashboard.blade.php
+++ resources/views/layouts/configDashboard.blade.php
@@ -111,11 +111,33 @@
                                             {{ $customer->full_name }}
                                         </div>
 
-                                        <div class="viewDetail">
+                                        <div class="viewDetail" data-toggle="modal" data-target="#user{{ $customer->id }}">
                                             Detalhes
                                             <i class="fa-solid fa-info-circle"></i>
                                         </div>
                                     </div>
+                                    <!-- Mostrar modal para cada usuario -->
+                                        <!-- Modal -->
+                                        <div class="modal fade" id="user{{ $customer->id }}" tabindex="-1" role="dialog" aria-labelledby="TituloModalCentralizado" aria-hidden="true">
+                                        <div class="modal-dialog modal-dialog-centered" role="document">
+                                            <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="TituloModalCentralizado">Título do modal</h5>
+                                                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                                <span aria-hidden="true">&times;</span>
+                                                </button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>Id do usuário -> {{ $customer->id }}</p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                                                <button type="button" class="btn btn-primary">Salvar mudanças</button>
+                                            </div>
+                                            </div>
+                                        </div>
+                                        </div>
+                                     <!-- Mostrar modal para cada usuario -->
                                     @endforeach
                                 </div>
 
